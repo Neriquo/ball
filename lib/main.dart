3d56624a9dcd533e6c7563ba899b9fd7a6798ce5
shrad <shradhakallee@gmail.com>
2025-03-18 10:45:09 +0100

    VersionFin KALLEE Shradha

--- lib/main.dart
+++ lib/main.dart
@@ -40,21 +40,34 @@ class Ball extends StatefulWidget {
 class _BallState extends State<Ball> {
   int ballNumber =
       1; // Initialisation à 1, car les balles commencent à ball1.png
+  int emojiNumber = 1;
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: TextButton(
-        onPressed: () {
-          setState(() {
-            ballNumber =
-                Random().nextInt(5) + 1; // génère un nombre entre 1 et 5
-          });
-          print(
-              'Ball Number: $ballNumber'); // Affiche le numéro de la balle dans la console
-        },
-        child: Image.asset('images/ball$ballNumber.png'),
-      ),
+    return Stack(
+      children: [
+        // Ajout des emojis en haut à gauche
+        Positioned(
+          top: 50,
+          left: 50,
+          child: Image.asset('images/emoji$emojiNumber.jpeg',
+              width: 150, height: 150), // Affiche l'emoji
+        ),
+        // Centre de l'écran où la balle apparaît
+        Center(
+          child: TextButton(
+            onPressed: () {
+              setState(() {
+                ballNumber = Random().nextInt(5) + 1;
+                emojiNumber = Random().nextInt(3) + 1;
+              });
+              print(
+                  'Ball Number: $ballNumber, Emoji Number: $emojiNumber'); // Affiche le numéro de la balle et de l'emoji dans la console
+            },
+            child: Image.asset('images/ball$ballNumber.png'),
+          ),
+        ),
+      ],
     );
   }
 }
