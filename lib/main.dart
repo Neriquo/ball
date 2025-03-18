5234e2a96cff47d0a600effa7fb5b5cd53d613b5
leo_bg_cf <leobgely@gmail.com>
2025-03-18 09:50:27 +0100

    feat: add scaffold and header bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,26 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(
-      MaterialApp(
-        home: null,
+  MaterialApp(
+    home: BallPage(),
+  ),
+);
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+     appBar: AppBar(
+       backgroundColor: Colors.indigo,
+       title: Text(
+           "Ask me anything",
+           style: TextStyle(color: Colors.white70)
+       ),
+     ),
+      body: Container(
+
       ),
     );
+  }
+}
