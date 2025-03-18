61b617912031b62ebe55e8b38bdd913e0f5c19b1
hnaceur <hella.naceur@gmail.com>
2025-03-18 11:15:34 +0100

    FINAL VERSION HELLA NACEUR STS 1 MERCII

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,50 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() => runApp(MaterialApp(home: BallPage()));
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: Text('Ask Me Anything',
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            )),
+        backgroundColor: Colors.blue.shade900,
       ),
+      body: Ball(),
     );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+/*  Widget build(BuildContext context) {
+    return Center(
+        child: TextButton(
+            onPressed: () {}, child: Image.asset('images/ball1.png')));
+  } */
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: TextButton(
+      onPressed: () {
+        setState(() {
+          ballNumber = Random().nextInt(5) + 1;
+          print('ballNumber: $ballNumber');
+        });
+      },
+      child: Image.asset('images/ball$ballNumber.png'),
+    ));
+  }
+}
