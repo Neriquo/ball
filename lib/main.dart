8a023d66f943682f66083601bc6174647c8b12e7
Armen0 <hugodev04@gmail.com>
2025-03-18 11:11:38 +0100

    added Alert with info abt the game

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 import 'dart:math';
 
 void main() => runApp(
@@ -32,14 +33,38 @@ class _BallState extends State<Ball> {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: TextButton(
-        onPressed: () {
-          setState(() {
-            ballNumber = Random().nextInt(5) + 1;
-          });
-        },
-        child: Image.asset('images/ball$ballNumber.png'),
-      ),
+      child: Column(
+        children: [
+          Text(
+              "Click the question mark :)",
+            style: TextStyle(
+              fontSize: 25.0,
+            ),
+          ),
+          TextButton(
+            onPressed: () {
+              setState(() {
+                Alert(
+                  context: context,
+                  title: 'Informations about the game',
+                  desc: 'Name : 8 Ball Oracle\n'
+                      'Goal : Think of a question and click the tha ball, it will give you an anwser.\n'
+                      'Have fun :)',
+                ).show();
+              });
+            },
+            child: Icon(Icons.question_mark, size: 100,),
+          ),
+          TextButton(
+            onPressed: () {
+              setState(() {
+                ballNumber = Random().nextInt(5) + 1;
+              });
+            },
+            child: Image.asset('images/ball$ballNumber.png'),
+          ),
+        ],
+      )
     );
   }
 }
