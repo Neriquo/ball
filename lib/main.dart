badece8c143cb600342f6eba3d31f92d99994f22
Leo Tsou <leo@waynium.com>
2025-03-18 10:23:08 +0100

    leo ZOU ALIAS TSOU

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,54 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.blue.shade900,
+        title: Text(
+          'Ask Me Anything',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 0;
+
+  void changeBall() {
+    setState(() {
+      ballNumber = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+        onPressed: () {
+          changeBall();
+          print('I got clicked');
+          print('Ball number: $ballNumber');
+        },
+        child: Image.asset('images/ball$ballNumber.png'),
+      ),
+    );
+  }
+}
