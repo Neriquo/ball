8fdfef19090f8ff853774d4b867be6d433e49cc4
shrad <shradhakallee@gmail.com>
2025-03-18 10:14:01 +0100

    Version1 KALLEE Shradha

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,60 @@
 import 'package:flutter/material.dart';
+import 'dart:math'; // Import de la bibliothèque dart:math pour générer des nombres aléatoires
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: BallPage(),
+    );
+  }
+}
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: Text(
+          'Ask Me Anything',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.blue[900],
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber =
+      1; // Initialisation à 1, car les balles commencent à ball1.png
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+        onPressed: () {
+          setState(() {
+            ballNumber =
+                Random().nextInt(5) + 1; // génère un nombre entre 1 et 5
+          });
+          print(
+              'Ball Number: $ballNumber'); // Affiche le numéro de la balle dans la console
+        },
+        child: Image.asset('images/ball$ballNumber.png'),
       ),
     );
+  }
+}
