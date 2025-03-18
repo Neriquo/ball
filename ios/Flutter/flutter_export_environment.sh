75f4a8276ddbf3c6db329791410cfd44b3e4eed1
Arthur94R <arthurrondeau94@gmail.com>
2025-03-18 11:11:14 +0100

    Projet MagicBall

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/henrique/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/henrique/StudioProjects/ball"
+export "FLUTTER_ROOT=/Users/arthurrondeau/Documents/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/arthurrondeau/StudioProjects/ball"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
