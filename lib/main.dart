877f5aecae787eb6904192aae4de8d88dd126a15
Armen0 <hugodev04@gmail.com>
2025-03-18 11:24:50 +0100

    added comments

--- lib/main.dart
+++ lib/main.dart
@@ -33,15 +33,19 @@ class _BallState extends State<Ball> {
   @override
   Widget build(BuildContext context) {
     return Center(
+      // Create a centered column
       child: Column(
         children: [
+          // Add a text to the column and show it
           Text(
               "Click the question mark :)",
             style: TextStyle(
               fontSize: 25.0,
             ),
           ),
+          // Add button that shows an Icon to the column and show it
           TextButton(
+            // When button clicked, it shows an alert with the information about the game
             onPressed: () {
               setState(() {
                 Alert(
@@ -55,7 +59,9 @@ class _BallState extends State<Ball> {
             },
             child: Icon(Icons.question_mark, size: 100,),
           ),
+          // Add button that shows an image to the column and show it
           TextButton(
+            // When clicked, it shows a random image of the 8 ball oracle game
             onPressed: () {
               setState(() {
                 ballNumber = Random().nextInt(5) + 1;
