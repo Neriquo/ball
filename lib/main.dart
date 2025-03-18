223f16ec9631dd18dd124563a8fc43e9d7592ddb
arthur <arthurcovo75@gmail.com>
2025-03-18 10:46:26 +0100

    final arthur c plus beau

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,10 @@ class BallPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.blue,
       appBar: AppBar(
-        title: Text('Ask Me Anything'),
+        title: Text('Pose une question à la boule magique ! ', style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
         backgroundColor: Colors.blue[900],
+        centerTitle: true,
+        elevation: 5.0,
       ),
       body: Ball(),
     );
@@ -36,14 +38,33 @@ class _BallState extends State<Ball> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: TextButton(
-        onPressed: () {
-          print('I got clicked');
-          changeBallImage();
-        },
-        child: Image.asset('images/ball$ballNumber.png'),
-      ),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Text(
+            'Clique sur la boule magique pour obtenir une réponse',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 22, color: Colors.white, fontWeight: FontWeight.w500),
+          ),
+        ),
+        SizedBox(height: 20),
+        Center(
+          child: TextButton(
+            onPressed: () {
+              print('Bien cliqué');
+              changeBallImage();
+            },
+            child: Image.asset('images/ball$ballNumber.png', width: 250, height: 250),
+          ),
+        ),
+        SizedBox(height: 20),
+        Text(
+          'Appuie pour révéler ta destinée !',
+          style: TextStyle(fontSize: 20, color: Colors.white, fontStyle: FontStyle.italic),
+        ),
+      ],
     );
   }
 }
