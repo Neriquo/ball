c47c8a565483dd725a23f48869d216d9ebd38f09
David Soeiro-Vuong <113916056+davidsvSIUU@users.noreply.github.com>
2025-03-18 10:06:50 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,59 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() => runApp(const MagicBallApp());
+
+class MagicBallApp extends StatelessWidget {
+  const MagicBallApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: BallPage(),
+    );
+  }
+}
+
+class BallPage extends StatelessWidget {
+  const BallPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: const Text('Ask Me Anything'),
+        backgroundColor: Colors.blue[900],
+      ),
+      body: const Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  const Ball({super.key});
+
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+
+  void changeBallImage() {
+    setState(() {
+      ballNumber = Random().nextInt(5) + 1;
+    });
+    print('Ball number: $ballNumber');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+        onPressed: changeBallImage,
+        child: Image.asset('images/ball$ballNumber.png'),
       ),
     );
+  }
+}
